9cadacd59b9422797e880250b58a3f20907bf8dc
shivangiGotwala <shivangimarwadi3375@gmail.com>
2019-10-24 13:18:47 -0700

    commit

--- app/src/androidTest/java/example/com/shivangigotawalatest/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/example/com/shivangigotawalatest/ExampleInstrumentedTest.java
@@ -1,11 +1,15 @@
 package example.com.shivangigotawalatest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import example.com.shivangigotawalatest.Activity.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -21,23 +25,81 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//         Context of the app under test.
+//
+//
+//        Context appContext = InstrumentationRegistry.getTargetContext();
+//
+//        assertEquals("example.com.shivangigotawalatest", appContext.getPackageName());
+//
+//        onView(withId(R.id.cv_contact))
+//                .perform(click());
+//
+//        onView(withId(R.id.cv_work))
+//                .perform(click());
+//
+//        onView(withId(R.id.cv_education))
+//                .perform(click());
+//
+//        onView(withId(R.id.cv_language))
+//                .perform(click());
+//    }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    /**
+     * for splash activity
+     */
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void testSplashFlow() {
+        startIntent();
+        onView(withId(R.id.tv_name)).check(matches(isDisplayed()));
+    }
 
-        assertEquals("example.com.shivangigotawalatest", appContext.getPackageName());
+    /**
+     * for Main activity clicks
+     */
+    @Test
+    public void testClicks() {
+        onView(withId(R.id.cv_contact)).perform(click());
+        onView(withId(R.id.cv_work)).perform(click());
+        onView(withId(R.id.cv_education)).perform(click());
+        onView(withId(R.id.cv_language)).perform(click());
+        onView(withId(R.id.iv_x)).perform(click());
+        onView(withId(R.id.iv_contact_x)).perform(click());
+        onView(withId(R.id.iv_work_x)).perform(click());
+        onView(withId(R.id.iv_lang_x)).perform(click());
+    }
 
-        onView(withId(R.id.cv_contact))
-                .perform(click());
+    /**
+     * for Main activity cardviews
+     */
 
-        onView(withId(R.id.cv_work))
-                .perform(click());
+    @Test
+    public void testCards() {
+        onView(withId(R.id.tv_cont_linkedin_id)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_cont_skype_id)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_cont_email)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_cont_github)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_cont_city)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_work_Details)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_edu_Details)).check(matches(isDisplayed()));
+        onView(withId(R.id.rv_language_Details)).check(matches(isDisplayed()));
+    }
 
-        onView(withId(R.id.cv_education))
-                .perform(click());
 
-        onView(withId(R.id.cv_language))
-                .perform(click());
+    /**
+     * test Intent
+     */
+    private void startIntent() {
+        mActivityRule.launchActivity(intent);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+
+        }
     }
 }
